4a7c5a5ee7495f67e3a5a9cdcfb27872563ae92e
sravan <sravan_vempadapu@intuit.com>
2019-01-21 11:25:01 +0530

    name fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 const Header = ({ children }) => {
 	return (
 		<header className={styles.header}>
-			<h1 className={styles.logo}>Calender</h1>
+			<h1 className={styles.logo}>Calendar</h1>
 			{children}
 			<section className={styles.avatar}>
 				<img src="avatar.png" alt="avatar" />
